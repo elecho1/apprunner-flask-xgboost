72f3b35ffb329aac5501783eb0104fe5f696d2c0
Wang <lijewang@909c4ad010fe.ant.amazon.com>
2021-10-18 11:57:21 +0900

    Separated model_predict func.

--- main_app.py
+++ main_app.py
@@ -27,7 +27,6 @@ def predict():
     """
     こちらのサイトを参考にしています。https://qiita.com/fam_taro/items/1464c42324f15d7b8223
     """
-    global model
 
     response = {
         "success": False,
@@ -36,17 +35,24 @@ def predict():
 
     if request.get_json().get("feature"):
         feature = request.get_json().get("feature")
-
-        feature = np.array(feature)
-        if len(feature) != 2:
-            feature = feature.reshape((1, -1))
         
-        dfeature = xgb.DMatrix(feature)
-        response["prediction"] = model.predict(dfeature).tolist()
-
+        response["pred"] = model_predict(feature)
         response["success"] = True
 
     return jsonify(response)
+
+
+def model_predict(feature):
+    global model
+    feature = np.array(feature)
+    if len(feature) != 2:
+        feature = feature.reshape((1, -1))
+        
+    dfeature = xgb.DMatrix(feature)
+    pred = model.predict(dfeature)
+    pred_list = pred.tolist()
+
+    return pred_list
 
 
 if __name__ == "__main__":
