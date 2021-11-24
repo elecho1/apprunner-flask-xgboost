d81e25c953bcab8cc88a395f9a1c2d4e6e525741
Wang <lijewang@909c4ad010fe.ant.amazon.com>
2021-11-25 00:43:32 +0900

    added code to record logs.

--- main_app.py
+++ main_app.py
@@ -1,4 +1,6 @@
 from flask import Flask, jsonify, request
+import datetime
+import json
 import numpy as np
 import pickle
 import xgboost as xgb
@@ -25,6 +27,8 @@ def index():
 # "http://<ドメイン名>/api/v1/predict"へAPI呼び出しを行う際の動作
 @app.route("/api/v1/predict", methods=["POST"])
 def predict():
+    request_time = datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=9))) #リクエスト時刻
+
     response = {
         "success": False,
         "Content-Type": "application/json"
@@ -36,6 +40,8 @@ def predict():
         response["pred"] = model_predict(feature) # model_predict関数を使ってモデル予測
         response["success"] = True
 
+        print_result(request_time, feature, response["pred"]) # 予測結果をCloudWatchへ出力
+
     return jsonify(response)
 
 
@@ -43,7 +49,7 @@ def model_predict(feature):
     global model
     feature = np.array(feature)
     app.logger.debug(feature.shape)  # HTTPリクエストのfeatureのnp.ndarrayに変換
-    if len(feature.shape) != 2:  # もしデータが1つ（=1次元）であった場合
+    if len(feature.shape) == 1:  # もしデータが1つ（=1次元）であった場合
         feature = feature.reshape((1, -1))
         
     dfeature = xgb.DMatrix(feature)  # XGBoostのデータ形式に変換 
@@ -52,6 +58,41 @@ def model_predict(feature):
 
     return pred_list
 
+
+def print_result(request_time: datetime.datetime, feature: list, pred: list):
+    """
+    リクエストおよびレスポンスを、CloudWatch分析用にprintする関数。
+    CloudWatch Logs Insightsを用いて分析しやすいよう、json形式の文字列でprintする。
+
+    参考リンク：https://dev.classmethod.jp/articles/how-to-cloudwatch-logs-insights/
+    """
+    request_time_iso = request_time.isoformat()
+    if check_list_dim(feature) == 1:
+        feature = [feature]
+
+    content = dict()
+    content["type"] = "RESULT"
+    content["request_time"] = request_time_iso
+
+    # 予測結果を、特徴量と予測値のペアに整形する
+    predict_list = [{"feature": ftr, "pred": prd} for ftr, prd in zip(feature, pred)]
+    content["predict"] = predict_list
+
+    # json文字列への変換
+    # text = json.dumps(content, indent=4, separators=(',', ': '))
+    text = json.dumps(content)
+
+    # 結果の表示（そのままCloudWatchへ記録される）
+    print(text, flush=True)
+
+    return
+
+
+def check_list_dim(l: list) -> int:
+    l_np = np.array(l)
+
+    return len(l_np.shape)
+    
 
 if __name__ == "__main__":
     app.run()
